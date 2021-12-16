e815e756f3d7b36e00346ded47aa6bbeff80d5ef
jakob. spiel .køhler <jakobspielkohler@live.dk>
2021-12-16 08:52:36 +0100

    added PressureController

---
+++ WebApi/Controllers/PressureController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Pressures;
+using ServiceLayer.Dto;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PressureController : ControllerBase
+    {
+
+        private readonly IPressureService _pressureService;
+
+        public PressureController(IPressureService pressureService)
+        {
+            _pressureService = pressureService;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IEnumerable<PressureDto>> Get()
+        {
+            return await _pressureService.GetAllAsync();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet]
+        [Route("Latest")]
+        public async Task<PressureDto> GetLatest()
+        {
+            return await _pressureService.GetLatestAsync();
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task Post([FromBody] double value)
+        {
+           await _pressureService.SetAsync(value);
+        }
+    }
+}
